c58883fe50fbfa6a8895c8687dfdc815b95ce658
Alessandro Mercurio <alessandro.mercurio.external@ynap.com>
2023-11-03 14:51:17 +0100

    clearing App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,17 @@
-import { useContext, useState } from 'react';
 import Header from './components/header/header.component';
-import { Outlet } from 'react-router-dom';
 import Footer from './components/footer/footer.component';
+import { Outlet } from 'react-router-dom';
 import { ThemeProvider } from './contexts/theme.context';
-import './App.css';
 import { UserProvider } from './contexts/user.context';
+import './App.css';
 
 function App() {
-  // const [user, setUser] = useState<CurrentUserContextType>(null);
-  // const [dark, setDark] = useState<ThemeContextType>({ dark: false });
-  // const value = { dark, setDark };
-  // const toggleDarkMode = () => setDark(!dark);
-  // const toggleDark = () => {
-  //   console.log('toggle');
-  //   setDark(!dark);
-  // };
-
   return (
     <div className="App min-h-screen flex flex-col ">
       <ThemeProvider>
         <UserProvider>
           <Header />
-          <main className=' grow '>
+          <main className='grow'>
             <Outlet />
           </main>
           <Footer />
